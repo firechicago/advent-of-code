9c8f8ed30ef954d578ef7dfd983c9c1a99102fef
Chris Wand <chris@vermonster.com>
2015-12-07 10:10:58 -0500

    Day 4

---
+++ Day-4-Problem-1/calculate.rb
@@ -0,0 +1,13 @@
+require 'digest'
+
+def md5(index, input = 'iwrupvqb')
+  Digest::MD5.hexdigest(input + index.to_s)
+end
+
+index = 0
+hash = md5(index)
+until hash[0..4] == "00000"
+  index += 1
+  hash = md5(index)
+end
+puts index
