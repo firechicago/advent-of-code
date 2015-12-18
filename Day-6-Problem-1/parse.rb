98da3d986b30f183630f61dab1d7c9b4e75a2f4a
Chris Wand <chris@vermonster.com>
2015-12-18 16:11:38 -0500

    Day 6 Problem 1

---
+++ Day-6-Problem-1/parse.rb
@@ -0,0 +1,33 @@
+require 'pry'
+raw_data = File.readlines(ARGV[0])
+
+@lights = []
+1000.times do
+  @lights << [0] * 1000
+end
+
+def change(x1, y1, x2, y2, action)
+  lights_to_change = (x1..x2).to_a.product((y1..y2).to_a)
+  case action
+  when "on"
+    lights_to_change.each do |x,y|
+      @lights[x][y] = 1
+    end
+  when "off"
+    lights_to_change.each do |x,y|
+      @lights[x][y] = 0
+    end
+  when "toggle"
+    lights_to_change.each do |x,y|
+      @lights[x][y] = (@lights[x][y] + 1) % 2
+    end
+  end
+end
+
+raw_data.each do |instruction|
+  action = instruction.match(/(on|off|toggle)/).to_s
+  coordinates = instruction.scan(/\d+/).map(&:to_i)
+  change(*coordinates, action)
+end
+
+puts @lights.flatten.reduce(&:+)
