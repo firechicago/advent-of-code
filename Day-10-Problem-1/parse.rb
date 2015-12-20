eb6d9eaa1c4967765d440f3a40333891804bfdca
Chris Wand <chris@vermonster.com>
2015-12-20 16:52:25 -0500

    Day 10

---
+++ Day-10-Problem-1/parse.rb
@@ -0,0 +1,16 @@
+input = "1113222113"
+
+def look_and_say(string)
+  new_string = ""
+  groups = string.scan(/(\d)(\1*)/).map { |a| a.reduce(&:+) }
+  groups.each do |group|
+    new_string << group.length.to_s
+    new_string << group[0]
+  end
+  new_string
+end
+
+40.times do
+  input = look_and_say(input)
+end
+puts input.length
